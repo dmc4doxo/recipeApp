40c5fca6db9e8faa6763e5167f53e21db48ddf89
Mack Coulibaly <m4coul@gmail.com>
2022-06-27 16:49:10 -0700

    made it mobile app dimension

--- recipe.js
+++ recipe.js
@@ -119,7 +119,7 @@ function addMeal(mealData, random = false) {
 //Store meal id
 function addMealToLocalStorage(mealId) {
   const mealIds = getMealsFromLocalStorage();
-  if (mealIds.length >= 6) {
+  if (mealIds.length >= 5) {
     mealIds.shift();
     localStorage.setItem("mealIds", JSON.stringify([...mealIds, mealId]));
   } else {
